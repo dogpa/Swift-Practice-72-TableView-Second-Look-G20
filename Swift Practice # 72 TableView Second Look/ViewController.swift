98766ca1e778622e63bfeaac582659ddd79ac963
Dogpa <dogpa0130@gmail.com>
2021-09-07 13:39:36 +0800

    Initial Commit

---
+++ Swift Practice # 72 TableView Second Look/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 72 TableView Second Look
+//
+//  Created by Dogpa's MBAir M1 on 2021/9/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
